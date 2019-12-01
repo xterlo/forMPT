1beaca04c99cae7230506eb20f013efc366a49f1
xterlo <36822079+xterlo@users.noreply.github.com>
2019-12-01 16:36:57 +0300

    Add files via upload

--- FileEncoderForMPT/FileEncoder.Designer.cs
+++ FileEncoderForMPT/FileEncoder.Designer.cs
@@ -30,16 +30,19 @@
         {
             this.openFileDlg = new System.Windows.Forms.Button();
             this.saveFileDlg = new System.Windows.Forms.Button();
-            this.KeyGen = new System.Windows.Forms.Button();
             this.importFile = new System.Windows.Forms.TextBox();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.exportFile = new System.Windows.Forms.TextBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.KeyText = new System.Windows.Forms.TextBox();
             this.Encrypt = new System.Windows.Forms.Button();
             this.BackButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ApplyKeyButton = new System.Windows.Forms.Button();
+            this.KeyTextBox = new System.Windows.Forms.TextBox();
+            this.KeyGen = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -49,32 +52,26 @@
             this.openFileDlg.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.openFileDlg.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
             this.openFileDlg.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
-            this.openFileDlg.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.openFileDlg.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.openFileDlg.Location = new System.Drawing.Point(325, 20);
             this.openFileDlg.Name = "openFileDlg";
             this.openFileDlg.Size = new System.Drawing.Size(25, 20);
             this.openFileDlg.TabIndex = 0;
             this.openFileDlg.Text = "...";
             this.openFileDlg.UseVisualStyleBackColor = false;
+            this.openFileDlg.Click += new System.EventHandler(this.OpenFileDlg_Click);
             // 
             // saveFileDlg
             // 
-            this.saveFileDlg.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.saveFileDlg.Enabled = false;
+            this.saveFileDlg.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.saveFileDlg.Location = new System.Drawing.Point(325, 60);
             this.saveFileDlg.Name = "saveFileDlg";
             this.saveFileDlg.Size = new System.Drawing.Size(25, 20);
             this.saveFileDlg.TabIndex = 1;
             this.saveFileDlg.Text = "...";
             this.saveFileDlg.UseVisualStyleBackColor = true;
-            // 
-            // KeyGen
-            // 
-            this.KeyGen.Location = new System.Drawing.Point(6, 42);
-            this.KeyGen.Name = "KeyGen";
-            this.KeyGen.Size = new System.Drawing.Size(344, 47);
-            this.KeyGen.TabIndex = 2;
-            this.KeyGen.Text = "Генерация ключа";
-            this.KeyGen.UseVisualStyleBackColor = true;
+            this.saveFileDlg.Click += new System.EventHandler(this.SaveFileDlg_Click);
             // 
             // importFile
             // 
@@ -118,54 +115,102 @@
             this.exportFile.Text = "Экспорт";
             this.exportFile.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.KeyText);
-            this.groupBox2.Controls.Add(this.KeyGen);
-            this.groupBox2.Location = new System.Drawing.Point(12, 126);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(362, 95);
-            this.groupBox2.TabIndex = 5;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Ключ";
-            // 
-            // KeyText
-            // 
-            this.KeyText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.KeyText.Location = new System.Drawing.Point(8, 16);
-            this.KeyText.Name = "KeyText";
-            this.KeyText.Size = new System.Drawing.Size(342, 20);
-            this.KeyText.TabIndex = 0;
-            // 
             // Encrypt
             // 
-            this.Encrypt.Location = new System.Drawing.Point(18, 228);
+            this.Encrypt.Enabled = false;
+            this.Encrypt.Location = new System.Drawing.Point(20, 228);
             this.Encrypt.Name = "Encrypt";
-            this.Encrypt.Size = new System.Drawing.Size(344, 43);
+            this.Encrypt.Size = new System.Drawing.Size(162, 43);
             this.Encrypt.TabIndex = 6;
             this.Encrypt.Text = "Зашифровать";
             this.Encrypt.UseVisualStyleBackColor = true;
+            this.Encrypt.Click += new System.EventHandler(this.Encrypt_Click);
             // 
             // BackButton
             // 
             this.BackButton.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.BackButton.Location = new System.Drawing.Point(20, 278);
+            this.BackButton.Location = new System.Drawing.Point(20, 307);
             this.BackButton.Name = "BackButton";
-            this.BackButton.Size = new System.Drawing.Size(75, 23);
+            this.BackButton.Size = new System.Drawing.Size(73, 23);
             this.BackButton.TabIndex = 7;
             this.BackButton.Text = "Назад";
             this.BackButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.BackButton.UseVisualStyleBackColor = true;
             this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ApplyKeyButton);
+            this.groupBox2.Controls.Add(this.KeyTextBox);
+            this.groupBox2.Controls.Add(this.KeyGen);
+            this.groupBox2.Location = new System.Drawing.Point(12, 126);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(362, 95);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ключ";
+            // 
+            // ApplyKeyButton
+            // 
+            this.ApplyKeyButton.Enabled = false;
+            this.ApplyKeyButton.Location = new System.Drawing.Point(191, 42);
+            this.ApplyKeyButton.Name = "ApplyKeyButton";
+            this.ApplyKeyButton.Size = new System.Drawing.Size(159, 47);
+            this.ApplyKeyButton.TabIndex = 3;
+            this.ApplyKeyButton.Text = "Применить";
+            this.ApplyKeyButton.UseVisualStyleBackColor = true;
+            this.ApplyKeyButton.Click += new System.EventHandler(this.ApplyKeyButton_Click);
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.KeyTextBox.Enabled = false;
+            this.KeyTextBox.Location = new System.Drawing.Point(8, 16);
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.Size = new System.Drawing.Size(342, 20);
+            this.KeyTextBox.TabIndex = 0;
+            // 
+            // KeyGen
+            // 
+            this.KeyGen.Enabled = false;
+            this.KeyGen.Location = new System.Drawing.Point(8, 42);
+            this.KeyGen.Name = "KeyGen";
+            this.KeyGen.Size = new System.Drawing.Size(162, 47);
+            this.KeyGen.TabIndex = 2;
+            this.KeyGen.Text = "Сгенерировать ключ";
+            this.KeyGen.UseVisualStyleBackColor = true;
+            this.KeyGen.Click += new System.EventHandler(this.KeyGen_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Enabled = false;
+            this.decryptButton.Location = new System.Drawing.Point(203, 228);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(159, 43);
+            this.decryptButton.TabIndex = 9;
+            this.decryptButton.Text = "Расшифровать";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(20, 277);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(342, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 10;
+            // 
             // FileEncoder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(386, 305);
+            this.ClientSize = new System.Drawing.Size(386, 346);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.BackButton);
             this.Controls.Add(this.Encrypt);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "FileEncoder";
             this.Text = "Файл";
@@ -182,15 +227,18 @@
 
         private System.Windows.Forms.Button openFileDlg;
         private System.Windows.Forms.Button saveFileDlg;
-        private System.Windows.Forms.Button KeyGen;
         private System.Windows.Forms.TextBox importFile;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox exportFile;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox KeyText;
         private System.Windows.Forms.Button Encrypt;
         private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button ApplyKeyButton;
+        private System.Windows.Forms.TextBox KeyTextBox;
+        private System.Windows.Forms.Button KeyGen;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- FileEncoderForMPT/FileEncoder.cs
+++ FileEncoderForMPT/FileEncoder.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
 
 namespace FileEncoderForMPT
 {
     public partial class FileEncoder : Form
     {
+        public static string keyForEncoder = "";
+        public static string importFileName;
+        public static string exportFileName;
         public FileEncoder()
         {
             InitializeComponent();
@@ -27,6 +32,145 @@ namespace FileEncoderForMPT
             MainMenu mainMenu = new MainMenu();
             mainMenu.Show();
             Hide();
+        }
+
+        private void ApplyKeyButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(KeyTextBox.Text);
+            if (KeyTextBox.Text.Length >= 4 && KeyTextBox.Text.Length <= 32)
+            {
+                keyForEncoder = KeyTextBox.Text;
+                Encrypt.Enabled = true;
+                decryptButton.Enabled = true;
+                MessageBox.Show("Ключ успешно установлен", "Успех");
+            }
+            else
+            {
+                MessageBox.Show("Ключ должен состоять из 4-32 символов","Ошибка");
+                Encrypt.Enabled = false;
+            }
+
+        }
+
+        private void KeyGen_Click(object sender, EventArgs e)
+        {
+            string alphabetS = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm=!@#$%^&*;><:";
+            char[] alphabet = alphabetS.ToCharArray();
+            Random rnd = new Random();
+
+            int countLetters = rnd.Next(10, 32);
+            string key = "";
+            for (int i = 0; i < countLetters; i++)
+            {
+                int letterNumber = rnd.Next(alphabet.Length);
+                key = key + alphabet[letterNumber];
+            }
+            KeyTextBox.Text = key;
+            ApplyKeyButton_Click(sender, e);
+            Encrypt.Enabled = true;
+            decryptButton.Enabled = true;
+        }
+
+        private void OpenFileDlg_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = null;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                importFileName = openFileDialog1.FileName;
+                importFile.Text = importFileName;
+                openFileDialog1.FileName = null;
+                openFileDialog1.RestoreDirectory = true;
+                saveFileDlg.Enabled = true;
+            }
+        }
+
+        private void SaveFileDlg_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                exportFileName = saveFileDialog1.FileName;
+                exportFile.Text = exportFileName;
+                saveFileDialog1.FileName = null;
+                saveFileDialog1.RestoreDirectory = true;
+                KeyGen.Enabled = true;
+                KeyTextBox.Enabled = true;
+                ApplyKeyButton.Enabled = true;
+            }
+        }
+
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+
+            string text = Encoding.GetEncoding(1252).GetString(File.ReadAllBytes(importFileName));        
+            string key = keyForEncoder;
+            int bs64Lenght = text.Length;
+            string firstHalf = text.Substring(0, bs64Lenght / 2); 
+            string secondHalf = text.Substring(bs64Lenght / 2);
+            string mergeText = secondHalf + firstHalf; 
+            string tempText;
+            string temp64 = ""; 
+            for (int i = 0; i < bs64Lenght - 1; i++)
+            {
+
+                tempText = Convert.ToString(mergeText[i + 1]) + Convert.ToString(mergeText[i]);
+                i++;
+                temp64 = temp64 + tempText;
+            }
+            char[] keyChars = key.ToCharArray();
+            char[] textChars = temp64.ToCharArray();
+            string lastText = "";
+            int temp = 0;
+            for (int i = 0; i < textChars.Length; i++)
+            {
+                if (temp >= keyChars.Length) temp = 0;
+                int tempChar = (int)textChars[i] + (int)keyChars[temp];
+                temp++;
+                char lastTextChar = (char)tempChar;
+                lastText = lastText + lastTextChar.ToString();
+            }
+            File.WriteAllText(exportFileName+".encrypted", lastText);
+            MessageBox.Show("Файл успешно зашифрован", "Успех");
+
+            
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                char[] keyChars = keyForEncoder.ToCharArray();
+                string text = File.ReadAllText(importFileName);
+                string fText = "";
+                int temp = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (temp >= keyChars.Length) temp = 0;
+                    int textCharsS = (int)text[i] - (int)keyChars[temp];
+                    temp++;
+                    char tempChar = (char)textCharsS;
+                    fText = fText + tempChar.ToString();
+                }
+
+                string temp64 = "";
+                for (int i = 0; i < fText.Length - 1; i++)
+                {
+
+                    string tempText = Convert.ToString(fText[i + 1]) + Convert.ToString(fText[i]);
+                    i++;
+                    temp64 = temp64 + tempText;
+                }
+
+                string firstHalf = temp64.Substring(0, temp64.Length / 2); 
+                string secondHalf = temp64.Substring(temp64.Length / 2);
+                string mergeTextf = secondHalf + firstHalf;            
+
+                File.WriteAllBytes(exportFileName + ".exe", Encoding.GetEncoding(1252).GetBytes(mergeTextf));
+                MessageBox.Show("Файл успешно расшифрован!", "Успех");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка, неверный ключ!","Ошибка");
+            }
         }
     }
 }
